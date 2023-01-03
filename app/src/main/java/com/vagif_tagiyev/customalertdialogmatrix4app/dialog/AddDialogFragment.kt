0415297d60ee147c80613f71146e7ba9cae7e3e2
Vaqif <taqiyevv937@gmail.com>
2023-01-03 22:07:16 +0400

    [MASTER] rview and button added to fragment main

--- app/src/main/java/com/vagif_tagiyev/customalertdialogmatrix4app/dialog/AddDialogFragment.kt
+++ app/src/main/java/com/vagif_tagiyev/customalertdialogmatrix4app/dialog/AddDialogFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.vagif_tagiyev.customalertdialogmatrix4app.R
-class AddDialogFragment : Fragment() {
-
+class AddDialogFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_add_dialog, container, false)
     }
 
